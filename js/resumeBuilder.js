495f027639e889aa4b442b92ae5b777d316863fa
Rohl Khan <rohailkhan@gmail.com>
2017-03-20 10:47:24 +1300

    adding bio,education and experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,68 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio ={
+	"Name": "Name : Rohail ",
+	"Role": "Role :Frontend Web developer",
+	"contacts": [
+		"mobile : 02102428830",
+		"email : rohailkhan@gmail.com",
+		"github : https://github.com/rohailkhan",
+		"location : Auckland"
+	],
+	"skills": ["My Skills ", "Android Development  ", "JavaScript  ", "HTML  ", "CSS  ", "Bootstrap  ", "Github  ", "NodeJs  "],
+	"biopic  ": "images/fry.jpg  "
+}
+
+var education={
+	"Educational Institutes": [{
+		"name": "University of Engineering and Technology",
+		"location": "Lahore ,Pakistan",
+		"degree": "Bachelor",
+		"majors": ["Electrical Engineering"],
+		"url": "www.uet.edu.pk"
+	}, {
+		"name": "UniTec",
+		"location": "Auckland",
+		"degree": "Short Course",
+		"majors": ["Programming one", "SQL"],
+		"url": "www.unitec.co.nz"
+	}],
+
+	"onlineCourses": [{
+		"title": "Java Programming fundamentals",
+		"Institute": "Duke University",
+		"website": "Coursera.org"
+	}, {
+		"title": "Java Programming fundamentals",
+		"Institute": "Duke University",
+		"website": "Coursera.org"
+	}] }
+
+var work={
+	"Telecom Project Manager": [{
+		"company": "",
+		"location": "Lahore ,Pakistan",
+		"degree": "Bachelor",
+		"majors": ["Electrical Engineering"],
+		"url": "www.uet.edu.pk"
+	}, {
+		"name": "UniTec",
+		"location": "Auckland",
+		"degree": "Short Course",
+		"majors": ["Programming one", "SQL"],
+		"url": "www.unitec.co.nz"
+	}],
+
+	"onlineCourses": [{
+		"title": "Java Programming fundamentals",
+		"Institute": "Duke University",
+		"website": "Coursera.org"
+	}, {
+		"title": "Java Programming fundamentals",
+		"Institute": "Duke University",
+		"website": "Coursera.org"
+	}] }
+
+var projects={
+	"title": "Miwak Language Application",
+	"platform": "Android",
+	"description": "Multi page Android Application for translation of some typical Miwak Language word"
+}
