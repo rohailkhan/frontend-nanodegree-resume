fe4e8e5c6d9cd35be8473c58108de3134d854271
Rohl Khan <rohailkhan@gmail.com>
2017-03-21 23:44:48 +1300

    initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,29 +36,31 @@ var education={
 	}] }
 
 var work={
-	"Telecom Project Manager": [{
-		"company": "",
-		"location": "Lahore ,Pakistan",
-		"degree": "Bachelor",
-		"majors": ["Electrical Engineering"],
-		"url": "www.uet.edu.pk"
-	}, {
-		"name": "UniTec",
-		"location": "Auckland",
-		"degree": "Short Course",
-		"majors": ["Programming one", "SQL"],
-		"url": "www.unitec.co.nz"
-	}],
-
-	"onlineCourses": [{
-		"title": "Java Programming fundamentals",
-		"Institute": "Duke University",
-		"website": "Coursera.org"
-	}, {
-		"title": "Java Programming fundamentals",
-		"Institute": "Duke University",
-		"website": "Coursera.org"
-	}] }
+	"jobs": [{
+			"employer": "Localize",
+			"title": "Senior Software Developer",
+			"location": "Edmonton, Alberta, Canada",
+			"datesWorked": "March 2015 - December 2015",
+			"description": "I was hired to work on Localize's online system, which involved knowledge in C# development, jQuery"
+		}, {
+			"employer": "Epcor",
+			"title": "Systems Analyst",
+			"location": "Edmonton, Alberta, Canada",
+			"datesWorked": "September 2003 - April 2006",
+			"description": "I worked in a few different capacities here from internal application support of "
+		}, {
+			"employer": "Matrikon",
+			"title": "Programmer",
+			"location": "Edmonton, Alberta, Canada",
+			"datesWorked": "March 2002 - September 2003",
+			"description": "I developed software for external clients in a variety of languages including C#."
+		}, {
+			"employer": "University of Windsor",
+			"title": "Teaching Assisstant / Computer Centre Support / Computer Science Tutor",
+			"location": "Windsor, Ontario, Canada",
+			"datesWorked": "September 1998 - April 2001",
+			"description": "I supervised labs for the second year advanced programming class in C as a teaching "
+		}	] }
 
 var projects={
 	"title": "Miwak Language Application",
@@ -85,4 +87,7 @@ $("#header").append(formatedHTMLwelcomeMsg);
 //using if statement for bio.skills and using them in HTMLskillsStart and HTMLskills
 if (bio.skills.length>0){
 	$("#header").append(HTMLskillsStart);
+	for (i in bio.skills){
+		$("#oye").append(HTMLskills.replace("%data%", bio.skills[i]));
+	}
 }
